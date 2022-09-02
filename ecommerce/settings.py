bc80db7ff11f9fc9e4ce19724433c54acda04c40
diyashrestha1999 <diyashrestha1999@gmail.com>
2022-09-02 16:41:12 +0545

    added staticfiles dirs

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!f9s+yfb&9_h2-3mojpgyb_prlv!^dnmjeie$a*jr)n1=@a)_@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
